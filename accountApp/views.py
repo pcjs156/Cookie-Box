53b5b6e772bbb5f51209fc25bae422ff1f89fc77
yubin <itqkrdbqls1001@naver.com>
2020-09-30 16:33:09 +0900

    이메일 인증 코드 재발급 기능 구현
    이메일을 변경하는 로직이 없는 점을 제외하고 email_modifying과 비슷하게 구현됨

--- accountApp/views.py
+++ accountApp/views.py
@@ -10,9 +10,92 @@ from .models import User, UserProfile
 from Cookie_Box.settings import DEBUG
 
 
+# 인증 코드 재발급을 요청하는 페이지
+# 이미 이메일 인증이 완료된 경우 사용할 수 없음
+# 마지막 인증 코드 재발급, 또는 회원가입으로부터 1시간이 지나야 다시 요청할 수 있음
+def auth_code_reissue_view(request, username: str):
+    content = dict()
+
+    # 이미 해당 계정이 이메일 인증이 되어 있는 경우를 위한 플래그
+    content['already_authenticated'] = False
+    # 모든 조건이 성립해 인증 코드 재발급이 가능한 경우를 위한 플래그
+    content['reissue_possible'] = True
+    # 비밀번호가 틀렸을 경우를 위한 플래그
+    content['wrong_password'] = False
+
+    # 요청을 받은 경우
+    if request.method == "POST":
+        password = request.POST.get("password")
+
+        user: User = authenticate(
+            request=request,
+            username=username,
+            password=password,
+        )
+
+        # 로그인에 실패했을 경우
+        if not user:
+            content['wrong_password'] = True
+
+        # 로그인에 성공한 경우
+        # 입력된 인증 코드 재발급하고, 이메일을 전송한다.
+        else:
+            profile = UserProfile.get_profile_by_username(username)
+            # 인증 코드를 갱신
+            profile.set_auth_code()
+            # 마지막으로 인증 코드 재발급 요청을 보낸 시간을 갱신
+            profile.last_email_modified = timezone.now()
+            profile.save()
+
+            # 인증 코드 전송
+            profile.send_auth_code()
+
+            return redirect("unauthenticated", username)
+    else:
+        user: User = User.objects.get(username=username)
+        profile: UserProfile = UserProfile.get_profile(user)
+
+        # 이메일 인증을 이미 완료한 경우 인증 코드 재발급 할 수 없음
+        if profile.email_authenticated:
+            content['already_authenticated'] = True
+
+        # 이메일 인증을 완료하지 않은 경우
+        else:
+            # 마지막 인증 코드 재발급 시각을 확인
+            last_auth_code_request = profile.last_auth_code_request
+            difference_sec = (timezone.localtime() -
+                              last_auth_code_request).total_seconds()
+
+            HOUR_SEC = 60**2
+            # 디버깅모드인 경우 : 마지막 갱신시간으로부터 5초가 지났을 경우 가능
+            if DEBUG is True:
+                # 인증 코드 재발급이 불가능한 경우
+                if difference_sec < 5:
+                    content['reissue_possible'] = False
+                    content['last_modifying'] = last_auth_code_request
+                # 인증 코드 재발급이 가능한 경우
+                else:
+                    content['email'] = user.email
+                    content['username'] = user.username
+                    content['last_modifying'] = last_auth_code_request
+            # 디버깅모드가 아닌 경우 : 마지막 인증 코드 재발급 시각과 현재 시각의 차이가 1시간 미만인 경우 가능
+            else:
+                # 인증 코드 재발급 불가능한 경우
+                if difference_sec < HOUR_SEC:
+                    content['reissue_possible'] = False
+                    content['last_modifying'] = last_auth_code_request
+                # 인증 코드 재발급이 가능한 경우
+                else:
+                    content['email'] = user.email
+                    content['username'] = user.username
+                    content['last_modifying'] = last_auth_code_request
+
+    return render(request, 'auth_code_reissue.html', content)
+
+
 # 이메일 주소 변경을 요청하는 페이지
 # 이미 이메일 인증이 완료된 경우 사용할 수 없음
-# 마지막 이메일 인증, 또는 회원가입으로부터 1시간이 지나야 다시 요청할 수 있음
+# 마지막 이메일 주소 변경, 또는 회원가입으로부터 1시간이 지나야 다시 요청할 수 있음
 def email_modify_view(request, username: str):
     content = dict()
 
