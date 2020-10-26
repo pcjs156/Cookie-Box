731d697e5725f3493f463b84a2d665922bd8a36f
yubin <itqkrdbqls1001@naver.com>
2020-10-27 02:09:52 +0900

    1. .gitignore에 pycharm 관련 파일 추가 2. Cookie_Box/settings.py의 ALLOWED_HOSTS에 루프백 IP 추가

--- Cookie_Box/settings.py
+++ Cookie_Box/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['cookiebox.pythonanywhere.com']
+ALLOWED_HOSTS = ['cookiebox.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
