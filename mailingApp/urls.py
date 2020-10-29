32fe3abb5fa04da9929d5742e04551c60e41eaab
yubin <itqkrdbqls1001@naver.com>
2020-10-29 20:48:02 +0900

    1. 이메일링 테스트 페이지 생성 2. 즉시 이메일을 발송하는 테스트 코드 작성

--- mailingApp/urls.py
+++ mailingApp/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('test/', views.email_test_view, name="email_test"),
+    path('test/instant_mail/', views.instant_mail_send_test_view, name="instant_mail_send_test"),
     path('report/', views.report_view, name="report"),
     path('schedule/', views.schedule_view, name="schedule"),
     path('statistics/', views.statistics_view, name="statistics"),
-    path('subscription_list/',
-         views.subscription_list_view,
-         name="subscription_list"),
+    path('subscription_list/', views.subscription_list_view, name="subscription_list"),
 ]
--- mailingApp/views.py
+++ mailingApp/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+from Cookie_Box.settings import DEBUG, EMAIL_HOST_USER
+
+# 아래는 테스트용 코드로, DEBUG 모드가 True이며 접속 계정이 staff일 때만 작동합니다.
+@login_required(login_url='/account/logIn/')
+def email_test_view(request):
+    content = dict()
+
+    if not DEBUG or not request.user.is_staff:
+        content["DEBUG"] = False
+
+    else:
+        content["DEBUG"] = True
+
+    return render(request, "email_test.html", content)
+
+@login_required(login_url='/account/logIn/')
+def instant_mail_send_test_view(request):
+    content = dict()
+
+    if not DEBUG or not request.user.is_staff:
+        content["DEBUG"] = False
+
+    else:
+        content["DEBUG"] = True
+        if request.method == "POST":
+            # 수신 이메일 주소
+            receiver_address = request.POST["email_address"]
+            # 메일 제목
+            mail_subject = "테스트 메일: 즉시 보내기"
+            # 메일 내용: test_mail.html
+            mail_content = render_to_string("test_mail.html")
+            # TODO: 이메일 템플릿이 제대로 첨부되지 않음(태그가 노출됨)
+            # 이메일 객체 생성 / 전송
+            email = EmailMultiAlternatives(subject=mail_subject, from_email=EMAIL_HOST_USER,
+                                           to=[receiver_address], body='??')
+            email.attach_alternative(mail_content, "text/html")
+            email.send()
+
+    return render(request, "instant_mail_send_test.html", content)
+
+# ========================================================
 
 #  신고 내역 관리 페이지
 @login_required(login_url='/account/logIn/')
